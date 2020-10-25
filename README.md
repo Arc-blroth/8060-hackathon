f8a1c0647e45725f2d9b44c6cf955e9deb971907
Glitch (hello-express) <none>
2020-10-25 00:41:02 +0000

    🐗🍪 Checkpoint ./web/index.html:9269157/474:24232794/53:24233303/31 ./web/script.js:9269157/2657:24232794/87:24233257/1198:24233259/13:24233303/40 ./server.js:24232794/14 ./README.md:24233319/2060

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 A server that serves a webpage, its resources, and some data
 
-
 ## Your Project
 
 On the front-end,
@@ -11,15 +10,16 @@ On the front-end,
 - `public/client.js` is the javacript that runs when you load the webpage
 - `public/style.css` is the styles for `views/index.html`
 - Drag in `assets`, like images or music, to add them to your project
+- u see stuff
 
 On the back-end,
 
 - your app starts at `server.js`
 - add frameworks and packages in `package.json`
 - safely store app secrets in `.env` (nobody can see this but you and people you invite)
+- u dont see stuff
 
 Click `Show` in the header to see your app live. Updates to your code will instantly deploy.
-
 
 ## Made by [Glitch](https://glitch.com/)
 
--- web/script.js
+++ web/script.js
@@ -1,41 +1,40 @@
-// client-side js, loaded by index.html
-// run by the browser each time the page is loaded
+console.log("are a winnin son");
 
-console.log("hello world :o");
 
-// define variables that reference elements on our page
-const dreamsList = document.getElementById("dreams");
-const dreamsForm = document.querySelector("form");
 
-// a helper function that creates a list item for a given dream
-function appendNewDream(dream) {
-  const newListItem = document.createElement("li");
-  newListItem.innerText = dream;
-  dreamsList.appendChild(newListItem);
+var ctx = document.getElementById("myChart").getContext("2d");
+var chart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: "bar",
+
+  // The data for our dataset
+  data: {
+    labels: ["Swag Wins"],
+    datasets: [
+      {
+        label: "Fortnite Dub Percentage",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [732]
+      }
+    ]
+  },
+
+  // Configuration options go here
+  options: {}
+});
+
+getStuff("/data/2791_2019dar.csv", (e) => {
+  console.log(e);
+});
+
+
+function getStuff(url, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", url, true); // true for asynchronous 
+    xmlHttp.send(null);
 }
-
-// fetch the initial list of dreams
-fetch("/dreams")
-  .then(response => response.json()) // parse the JSON from the server
-  .then(dreams => {
-    // remove the loading text
-    dreamsList.firstElementChild.remove();
-  
-    // iterate through every dream and add it to our page
-    dreams.forEach(appendNewDream);
-  
-    // listen for the form to be submitted and add a new dream when it is
-    dreamsForm.addEventListener("submit", event => {
-      // stop our form submission from refreshing the page
-      event.preventDefault();
-
-      // get dream value and add it to the list
-      let newDream = dreamsForm.elements.dream.value;
-      dreams.push(newDream);
-      appendNewDream(newDream);
-
-      // reset form
-      dreamsForm.reset();
-      dreamsForm.elements.dream.focus();
-    });
-  });
