cd365ea02ec03346906417af622aa77af797529e
Glitch (8060-hackathon) <none>
2020-11-02 05:20:06 +0000

    🌃🎰 Updated with Glitch

--- js/init.js
+++ js/init.js
@@ -10,7 +10,7 @@ const datasets = [
 ];
 
 let lastElement = null;
-let previousTeamOptions = [];
+let previousCharts = [];
 let dataset = null;
 let teamNumber = 0;
 let field = 0;
@@ -54,106 +54,297 @@ function init() {
         }
         
         let teamSelect = document.getElementById("team");
-        let statSelect = document.getElementById("stat");
         
-        let updateOtherOptions = function() {
-            
-            // document.body.style.backgroundImage = `url("https://cdn.discordapp.com/avatars/426932991885967360/aba9162a3baf0d45bd21f1255187ae13.png?size=256)`;
-                
-            previousTeamOptions.forEach(o => o.parentElement.removeChild(o));
-            previousTeamOptions = [];
+        let updateOtherOptions = () => {    
             let teams = Object.keys(allData[dataset].teamData);
-            for(let i = 0; i < teams.length; i++) {
-              let option = document.createElement('option');
-              option.text = teams[i];
-              option.value = teams[i];
-              teamSelect.appendChild(option);
-              previousTeamOptions.push(option);
+          
+            for(let i = teamSelect.options.length - 1; i >= 1; i--) {
+              teamSelect.remove(i);
             }
-                    
-            let stats = allData[dataset].headers;
-            for(let i = 0; i < stats.length; i++) {
-              statSelect.options[i + 1] = new Option(allData[dataset].headers[i], stats[i]);
+            
+            for(let i = 0; i < teams.length; i++) {
+              let option = new Option(teams[i], teams[i]);
+              teamSelect.options[i + 1] = option;
             }
           
         };
       
-       /*let updateImage = function(){
-         if(dataset="2791_2019dar"){
-           document.body.style.backgroundImage = 'url(https://www.firstinspires.org/sites/default/files/uploads/frc/Blog/2019-frc-game-logo-small.jpg)';
-         }
-         else if(dataset = "3476_2015cc"){
-           document.body.style.backgroundImage = 'url(https://www.firstinspires.org/sites/default/files/uploads/frc/Blog/2019-frc-game-logo-small.jpg)';
-         }
-         else if(dataset ="3476_2018cc" ){
-           document.body.style.backgroundImage = 'url(https://www.firstinspires.org/sites/default/files/uploads/frc/Blog/2019-frc-game-logo-small.jpg)';
-         }
-         else if(dataset = "3476_2018roe"){
-           document.body.style.backgroundImage = 'url(https://www.firstinspires.org/sites/default/files/uploads/frc/Blog/2019-frc-game-logo-small.jpg)';
-         }
-         else{
-           document.body.style.backgroundImage = 'url(https://www.firstinspires.org/sites/default/files/uploads/frc/Blog/2019-frc-game-logo-small.jpg)';
+        let eighteen='url(https://www.firstinspires.org/sites/default/files/uploads/frc/Blog/2019-frc-game-logo-small.jpg)';
+      
+       let updateImage = () => {
+         if(dataset == "2791_2019dar"){
+           setBackgroundImage("https://www.firstinspires.org/sites/default/files/uploads/frc/Blog/2019-frc-game-logo-small.jpg", 0.50);
+         } else if(dataset == "3476_2015cc"){
+           setBackgroundImage("https://www.firstinspires.org/sites/default/files/uploads/frc/Blog/2019-frc-game-logo-small.jpg", 0.50);
+         } else if(dataset == "3476_2018cc" ){
+           setBackgroundImage("https://www.firstinspires.org/sites/default/files/uploads/frc/Blog/2019-frc-game-logo-small.jpg", 0.50);
+         } else if(dataset == "3476_2018roe"){
+           setBackgroundImage("https://www.firstinspires.org/sites/default/files/uploads/frc/Blog/2019-frc-game-logo-small.jpg", 0.50);
+         } else {
+           setBackgroundImage("https://www.firstinspires.org/sites/default/files/uploads/frc/Blog/2019-frc-game-logo-small.jpg", 0.50);
          }
        }
-       */
+       
+       function setBackgroundImage(imageid, opacity) {
+          var s = document.body.style;
+          //s.backgroundImage = `url(${imageid})`;
+       }
+       
         
         datasetSelect.onchange = function() {
             if (this.selectedIndex) {
                 dataset = this.value;
                 updateOtherOptions();
-                updateGraphs();
+                updateImage();
+                if(teamNumber) updateGraphs();
             }
         }
         
-        document.getElementById("teamSelect").addEventListener("change", (e) => {
-            if (e.target.value) {
-                teamNumber = e.target.value;
-                updateGraphs();
-            }
-          
-        });
-        
-        statSelect.onchange = function() {
+        teamSelect.onchange = function() {
             if (this.selectedIndex) {
-                field = this.selectedIndex - 1;
+                teamNumber = this.value;
                 updateGraphs();
             }
-        }
+        };
         
     });
 }
 
 function updateGraphs() {
     let teamData = allData[dataset].teamData[teamNumber];
-    let matches = [];
-    let data = [];
-    Object.keys(teamData).forEach(match => {
-        matches.push("Match " + match);
-        data.push(teamData[match][field]);
-    });
+    let chartData = buildChartData(teamData);
     
-    if(lastElement != null) {
-        lastElement.parentElement.removeChild(lastElement);
+    if(previousCharts.length > 0) {
+        previousCharts.forEach(e => e.parentElement.removeChild(e));
+        previousCharts = [];
     }
-    
-    lastElement = buildElement('canvas');
-    let ctx = lastElement.getContext('2d');
-    let chart = new Chart(ctx, {
-        type: 'bar',
+  
+    chartData.forEach(c => {
+      let ele = buildElement('canvas');
+      let ctx = ele.getContext('2d');
+      let chart = new Chart(ctx, {
+          type: 'bar',
 
-        // The data for our dataset
-        data: {
-            labels: matches,
-            datasets: [{
-                label: `Team ${teamNumber} ${allData["2791_2019dar"].headers[field]}`,
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: data
-            }]
-        },
+          // The data for our dataset
+          data: {
+              labels: c.labels,
+              datasets: [{
+                  label: c.title,
+                  backgroundColor: 'rgb(255, 99, 132)',
+                  borderColor: 'rgb(255, 99, 132)',
+                  data: c.data
+              }]
+          },
 
-        // Configuration options go here
-        options: {}
+          // Configuration options go here
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  stepSize: 1,
+                  beginAtZero: true,
+                },
+              }],
+            },
+          }
+      });
+      previousCharts.push(ele);
+      document.body.append(ele);
     });
-    document.body.append(lastElement);
+    
+}
+
+class ChartData {
+  constructor(title, labels, data) {
+    this.title = title;
+    this.labels = labels;
+    this.data = data;
+  }
+}
+
+const sum = (a, b) => Number(a) + Number(b);
+
+const yesNo = {
+  0: "No",
+  1: "Yes"
+}
+
+function buildChartData(teamData) {
+  let out = [];
+  if(dataset == datasets[0]) {
+    // 2019
+    out.push(countEnum(teamData, "Starting Position", 0, {
+        L1: "Left 1",
+        L2: "Left 2",
+        M:  "Middle",
+        R1: "Right 1",
+        R2: "Right 2"
+    }));
+    out.push(countEnum(teamData, "Habitat Line", 1, yesNo));
+    out.push(countNums(teamData, "Hatches Scored: Auto", {
+      2: "Ship Side",
+      3: "Ship Front"
+    }));
+    out.push(countNums(teamData, "Cargo Scored: Auto", {
+      4: "Ship Side"
+    }));
+    out.push(countNums(teamData, "Auto Cargo Rocket Level", {
+      5: "Level"
+    }));
+    out.push(countNums(teamData, "Auto Hatch Rocket Level", {
+      6: "Level "
+    }));
+    out.push(countNums(teamData, "Auto Number of Cargo Dropped", {
+      6: "Level"
+    }));
+    out.push(countNums(teamData, "Auto Number of Hatches Dropped", {
+      6: "Level"
+    }));
+    out.push(countNums(teamData, "Teleop Cargo Ship", {
+      11: "Level"
+    }));
+    out.push(countNums(teamData, "Teleop Rocket Hatch ", {
+      12: "Left 1",
+      13:
+      14:
+    }));
+  } else if(dataset == datasets[1]) {
+    // 2015
+    out.push(countEnum(teamData, "Autozone Totes", 0, {
+      0: "No Totes moved into Autozone during Autonomous period",
+      1: "One Totes moved into Autozone during Autonomous period",
+      2: "Two Totes moved into Autozone during Autonomous period",
+      3: "Three Totes moved into Autozone during Autonomous period",
+    }))
+    out.push(countEnum(teamData, "Moved Into Auto Zone", 1, yesNo))
+    out.push(countEnum(teamData, "Bins Retrieved from Step", 2, {
+      0: "No bins retrieved from step zone",
+      1: "One bins retrieved from step zone",
+      2: "Two bins retrieved from step zone",
+    }))
+    out.push(countEnum(teamData, "Stacks", 2, {
+      0: "No bins retrieved from step zone",
+      1: "One bins retrieved from step zone",
+      2: "Two bins retrieved from step zone",
+    }))
+  } else if(dataset == datasets[2]) {
+    // 2018cc
+    out.push(countEnum(teamData, "Crossed Line Autonomously?", 1, yesNo));
+    out.push(countNums(teamData, "Cubes Moved Autonomously", {
+      2: "Own Cubes on Switch",
+      3: "Own Cubes on Scales",
+    }));
+    out.push(countNums(teamData, "Cubes Moved w/ Teleop", {
+      4: "Own Cubes on Switch",
+      5: "Own Cubes on Scales",
+      6: "Opponent Cubes on Switch"
+    }));
+    out.push(countNums(teamData, "Exchanged Cubes",{
+      7: "Cubes Exchanged"
+    }));
+    out.push(countNums(teamData, "Climb Success", {
+      8: "Climbed Alone",
+      9: "Climbed w/ 1 other",
+      10: "Climbed w/ 2 others"
+    }));
+    out.push(countEnum(teamData, "Played Defense?", 11, yesNo));
+  } else if(dataset == datasets[3]) {
+    // 2018roe
+    out.push(countEnum(teamData, "Habitat Line", 1, yesNo));
+    out.push(countNums(teamData, "Cubes Moved Autonomously",  {
+      2: "Num of Own Cubes on Switch",
+      3: "Num of Own Cubes on Scale",
+    }));
+    out.push(countNums(teamData, "Cubes Moved w/ Teleop", {
+      4: "Num of Own Cubes on Switch",
+      5: "Num of Own Cubes on Scale",
+      6: "Num of Opponent Cubes on Switch"
+    }));
+    out.push(countNums(teamData, "Exchanged Cubes", {
+      7: "Cubes Exchanged"
+    }));
+    out.push(countEnum(teamData, "Did they play Defense?", 11, yesNo));
+    out.push(countNums(teamData, "Num of bots climbed", ))
+  } else if(dataset == datasets[4]) {
+    // 2020
+    
+    //column 1
+    out.push(countEnum(teamData, "Starting Location", 0, {
+      "Inline with Opposing Trench": "Opp. Trench",
+      "Right of Goal": "Right Goal",
+      "Left of Goal": "Left Goal"
+    }));
+    
+    //column 2-9
+    out.push(countNums(teamData, "Auto Balls", {
+      1: "Low",
+      2: "1",
+      3: "2/3",
+      4: "4",
+      5: "5",
+      6: "6",
+      7: "Acquired",
+      8: "End Location"
+    }));
+    
+    //column 10-15
+    out.push(countNums(teamData, "Tele Balls", {
+      9: "Low",
+      10: "1",
+      11: "2/3",
+      12: "4",
+      13: "5",
+      14: "6"
+    }));
+    
+    //column 16
+    out.push(countEnum(teamData, "Wheel Spin", 15, yesNo));
+    
+    //column 17
+    out.push(countEnum(teamData, "Wheel Color Match", 16, yesNo));
+    
+    //column 18
+    out.push(countEnum(teamData, "Defender", 17, {
+      "None": "None",
+      "Light": "Light",
+      "Heavy": "Heavy"
+    }));
+    
+    //column 19
+    out.push(countEnum(teamData, "Target", 18, {
+      "None": "None",
+      "Light": "Light",
+      "Heavy": "Heavy"
+    }));
+    
+  }
+  return out;
+}
+
+/**
+ * Counts data that is specified as an enum.
+ * Example: Starting Position can be L1, L2, M, R1, or R2
+ */
+function countEnum(teamData, title, field, map) {
+  let keys = Object.keys(map);
+  let out = new Array(keys.length).fill(0);
+  teamData.map(m => m[field]).forEach(f => {
+    for(let i = 0; i < keys.length; i++) {
+      if(f == keys[i]) {
+        out[i]++;
+        break;
+      }
+    }
+  });
+  return new ChartData(title, Object.values(map), out);
+}
+
+/**
+ * Counts several columns of related numerical data.
+ * Example: Number of Stacks 1, Number of Stacks 2, etc
+ */
+function countNums(teamData, title, map) {
+  console.log(teamData);
+  let out = Object.keys(map).map(k => teamData.flatMap(m => m[k]).reduce(sum));
+  return new ChartData(title, Object.values(map), out);
 }
--- js/load.js
+++ js/load.js
@@ -3,7 +3,8 @@
 ((scripts) => {
     function loadScript(scriptSrc) {
         return new Promise((resolve, reject) => {
-            scriptSrc = scriptSrc;
+            let module = typeof scriptSrc == "object" && scriptSrc.module;
+            scriptSrc = typeof scriptSrc == "string" ? scriptSrc : scriptSrc.src;
             let scriptTag = document.createElement("script");
             let whenLoaded = () => {
                 resolve(scriptSrc);
@@ -30,6 +31,7 @@
 })([
     "https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js",
     "https://code.createjs.com/1.0.0/createjs.min.js",
+    { src: "https://unpkg.com/scrawl-canvas@8.2.5", module: true },
     "/js/util.js",
     "/js/models.js",
     "/js/init.js",
